5370b305a36de4b4e4906a1e2157dc8375d4aae5
IMCORIN2 <k_yoon1121@naver.com>
2023-11-07 15:39:57 +0900

    env 추가

--- schemas/index.js
+++ schemas/index.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
 
+const dotenv = requiure("dotenv").config();
+
 const connect = () => {
   mongoose
-    .connect("mongodb+srv://dbscks95:DaDHdQC7ORZuhlmK@cluster0.mtqzt6c.mongodb.net/spa_mall")
+    .connect(
+      process.env.DB_URL,
+      {
+        dbName : 'node_lv1',
+      },
+    )
     //mongodb가 연결중에 에러가 발생하면 아래의 코드로 감
-    .catch(err => console.log(err));
+    .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
+    .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
 
 // mongodb가 연결된 후에 에러가 났을 경우
 mongoose.connection.on("error", err => {
-  console.error("몽고디비 연결 에러", err);
+  console.error("MongoDB 연결 에러", err);
 });
 
 module.exports = connect;
